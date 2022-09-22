b209f034e80fff84c26d27d97269541922d98898
ffs108 <franciscofigueroa108@gmail.com>
2022-09-21 20:26:47 -0700

    added torpedo lerp + koan fixes to inertia

--- Program1/Assets/Scripts/Guidance.cs
+++ Program1/Assets/Scripts/Guidance.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 
 /// <summary>
@@ -31,23 +34,23 @@ public class Guidance : MonoBehaviour
     [Tooltip("Direction of the torpedo")]
     public Vector3 direction = Vector3.zero;
 
-	[Tooltip("Maximum torpedo life in seconds")]
-	public float timeToLive = 6.0f;
+    [Tooltip("Maximum torpedo life in seconds")]
+    public float timeToLive = 6.0f;
 
-	[Tooltip("Time for torpedo to fade out in seconds")]
-	public float fadeTime = 0.5f;
+    [Tooltip("Time for torpedo to fade out in seconds")]
+    public float fadeTime = 0.5f;
 
-	GameObject[] potentialTargets = null;
-	GameObject targetObject = null;
-	Vector3    targetPoint  = Vector3.zero;
+    GameObject[] potentialTargets = null;
+    GameObject targetObject = null;
+    Vector3 targetPoint = Vector3.zero;
 
-	bool targetImpacted = false;
-	bool targetAcquired = false;
+    bool targetImpacted = false;
+    bool targetAcquired = false;
 
-	Vector3 startPos;
-	float t = 0.0f;
+    Vector3 startPos;
+    float t = 0.0f;
 
-	float tMax = 0.0f;
+    float tMax = 0.0f;
 
     // Acquire a new target point
     bool Acquire(out Vector3 target, ref GameObject targObj)
@@ -66,77 +69,110 @@ public class Guidance : MonoBehaviour
 
             if (candFound)
             {
-                found    = candFound;
-                target   = candTarget;
-				targObj  = potentialTargets[i];
+                found = candFound;
+                target = candTarget;
+                targObj = potentialTargets[i];
             }
         }
         return found;
     }
 
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-		// assemble a list of all targets this torpedo may acquire
-		potentialTargets = GameObject.FindGameObjectsWithTag("Targetable");
+        // assemble a list of all targets this torpedo may acquire
+        potentialTargets = GameObject.FindGameObjectsWithTag("Targetable");
 
-		startPos = transform.position;
-	}
+        startPos = transform.position;
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		// decrement the lifetime counter for this tick
-		timeToLive -= Time.deltaTime;
-		//Debug.Log("Time to live: " + timeToLive);
+    // Update is called once per frame
+    void Update()
+    {
+        // decrement the lifetime counter for this tick
+        timeToLive -= Time.deltaTime;
+        //Debug.Log("Time to live: " + timeToLive);
 
-		// move until contact with target or out of gas
-		if (!targetImpacted && timeToLive > 0.001f)
-		{
-			if (!targetAcquired)
-			{
-				targetAcquired = Acquire(out targetPoint, ref targetObject);
+        // move until contact with target or out of gas
+        if (!targetImpacted && timeToLive > 0.001f)
+        {
+            if (!targetAcquired)
+            {
+                targetAcquired = Acquire(out targetPoint, ref targetObject);
+                startPos = transform.position;
+                t = 0.0f;//reset
+                //Debug.Log("Target acquired: " + targetAcquired + " at position " + targetPoint);
+            }
 
-				//Debug.Log("Target acquired: " + targetAcquired + " at position " + targetPoint);
-			}
 
+            // Note: The targeting logic below is imperfect and can cause the
+            // torpodo to "miss" its target under certain circumstances. Why?
+            //
+            // Upgrade this code by using LERP to move to torpedo to its target
+            // and detect collisions.
 
-			// Note: The targeting logic below is imperfect and can cause the
-			// torpodo to "miss" its target under certain circumstances. Why?
-			//
-			// Upgrade this code by using LERP to move to torpedo to its target
-			// and detect collisions.
+            float currentDist = Mathf.Abs((transform.position - targetPoint).sqrMagnitude); //OLD
+            //float currentDist = (myLerp(startPos, targetPoint, (t+Time.deltaTime)/timeToLive)).sqrMagnitude;
+            //transform.position = myLerp(startPos, targetPoint, velocity*Time.deltaTime);
 
-			float currentDist = Mathf.Abs((transform.position - targetPoint).sqrMagnitude);
+            if (targetAcquired)
+            {
+                Vector3 distVec = targetPoint - startPos;
+                float length = Mathf.Sqrt(Mathf.Pow(distVec.x, 2) + Mathf.Pow(distVec.y, 2));
+                t += (velocity * Time.deltaTime) * 1f / length; //not timeToLive??
+                transform.position = startPos + t * distVec;
+                //transform.position = myLerp(startPos, targetPoint, t);
+            }
+            // Either there's no target, or we haven't yet impacted target.
+            if (!targetAcquired || currentDist > 0.002f)
+            {
+                // just move along direction of travel indefinitely
+                transform.Translate(direction * velocity * Time.deltaTime);
+            }
+            else if(t > 1) // we've hit an acquired target: reflect
+            {
+                targetImpacted = true;
 
-			// Either there's no target, or we haven't yet impacted target. 
-			if (!targetAcquired || currentDist > 0.002f)
-			{
-				// just move along direction of travel indefinitely
-				transform.Translate(direction * velocity * Time.deltaTime);
-			}
-			else // we've hit an acquired target: reflect
-			{
-				targetImpacted = true;
+                // contacted with targetable object; play ricochset sound
+                AudioSource ricochet = GetComponents<AudioSource>()[1];
+                ricochet.Play();
 
-				// contacted with targetable object; play ricochset sound
-				AudioSource ricochet = GetComponents<AudioSource>()[1];
-				ricochet.Play();
+                timeToLive = 0.5f;
+            }
+        }
 
-				timeToLive = 0.5f;
-			}
-		}
+        if (timeToLive <= fadeTime)
+        {
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
 
-		if (timeToLive <= fadeTime)
-		{
-			SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            // fade the torpedo out of existence
+            renderer.color = new Color(1.0f, 1.0f, 1.0f, (renderer.color.a - (Time.deltaTime / fadeTime)));
 
-			// fade the torpedo out of existence
-			renderer.color = new Color (1.0f, 1.0f, 1.0f, (renderer.color.a - (Time.deltaTime / fadeTime)));
+            if (timeToLive <= 0.001f)
+                // remove the object from the game
+                GameObject.Destroy(gameObject);
+        }
+    }
 
-			if (timeToLive <= 0.001f)
-				// remove the object from the game
-				GameObject.Destroy(gameObject);
-		}
+    /*
+	 *  
+	 * Added LERP functions here -ff
+	 * 
+	 */
+
+    //helper function --unneccesary
+    float lowerOrderLerp(float firstVal, float secondVal, float t)
+    {
+        return (1 - t) * firstVal + t * secondVal; // (1 - t) * v0 + t * v1
+    }
+
+    //hates my func but is ok when in the if statement?????
+    Vector3 myLerp(Vector3 start, Vector3 end, float t)
+    {
+        Vector3 retVal = end - start;
+        float length = Mathf.Sqrt(Mathf.Pow(retVal.x, 2) + Mathf.Pow(retVal.y, 2));
+        t += (velocity * Time.deltaTime) * 1f / length;
+        //retVal =* t + start;
+        return retVal;
     }
 }
